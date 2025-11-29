699abef1b45a58dd6779cc1c09f53fc09b4ab750
Shobhit rajvanshi <shobhitraj1998@gmail.com>
2025-11-29 22:25:30 +0530

    feat: implemented profile page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Home from "./pages/home/Home";
 // import Login from "./pages/login/Login";
-// import Profile from "./pages/profile/Profile";
+import Profile from "./pages/profile/Profile";
 // import Register from "./pages/register/Register";
 
 function App() {
-  return <Home />;
+  // return <Home />;
+  return <Profile />;
 }
 
 export default App;
